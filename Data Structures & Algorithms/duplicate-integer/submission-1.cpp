726b42695df8e3195152cc0cc3b18ad3435bf840
Daksh Desai <78773703+dakshdesai00@users.noreply.github.com>
2026-06-11 12:25:24 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto num: nums){
+            mp[num]++;
+            if(mp[num] >= 2) return true;
+        }
+        return false;
+    }
+};
