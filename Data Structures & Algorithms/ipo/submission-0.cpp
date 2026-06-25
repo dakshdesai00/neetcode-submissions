0fa86b9802c8f1da03fdf8c573f85f561e291052
Daksh Desai <78773703+dakshdesai00@users.noreply.github.com>
2026-06-25 17:47:19 +0530

    Add: ipo - submission-0

---
+++ Data Structures & Algorithms/ipo/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> projects;
+        for (int i = 0; i < profits.size(); i++)
+            projects.push_back({capital[i], profits[i]});
+
+        sort(projects.begin(), projects.end(), [](auto &a, auto &b) {
+            if (a.first == b.first) return a.second > b.second;
+            return a.first < b.first;
+        });
+
+        priority_queue<int> pq;
+        int i = 0, n = projects.size();
+
+        while (k--) {
+            while (i < n && projects[i].first <= w) {
+                pq.push(projects[i].second);
+                i++;
+            }
+
+            if (pq.empty()) break;
+
+            w += pq.top();
+            pq.pop();
+        }
+
+        return w;
+    }
+};
