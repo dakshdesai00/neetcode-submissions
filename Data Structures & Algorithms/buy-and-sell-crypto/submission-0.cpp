270fab5d80e3995678252ae6770eb426294b1f2b
Daksh Desai <78773703+dakshdesai00@users.noreply.github.com>
2026-07-06 14:00:11 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] - buyPrice > profit) profit = prices[i] - buyPrice;
+            else if(prices[i] < buyPrice) buyPrice = prices[i];
+        }
+        return profit;
+    }
+};
